b0c842a9ad05b4278dc05d36acd5d91ab6f34d29
raphael <raphael.saojudas@gmail.com>
2018-11-11 11:34:19 -0200

    aula05

---
+++ app/src/main/java/br/usjt/ads/desmob/clienteads18/controler/MainActivity.java
@@ -0,0 +1,62 @@
+package br.usjt.ads.desmob.clienteads18.controler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import br.usjt.ads.desmob.clienteads18.R;
+import br.usjt.ads.desmob.clienteads18.model.Cliente;
+import br.usjt.ads.desmob.clienteads18.model.ClienteDAO;
+
+public class MainActivity extends Activity {
+    public static final String CHAVE = "br.usjt.ads.desmob.clienteads18.controler.chave";
+    private EditText txtBusca;
+    public static final String URL = "/rest/clientes";
+    public static final String HOST = "http://198.162.1.56:8080/app_poetas";
+    public static final String CLIENTES = "br.usjt.ads.desmob.clienteads18.controller.clientes";
+    Intent intent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtBusca = findViewById(R.id.txt_busca);
+    }
+
+    public void buscarClientes(View view) {
+        Intent intent = new Intent(this, ListaClientesActivity.class);
+        String texto = txtBusca.getText().toString();
+        intent.putExtra(CHAVE, texto);
+        if(ClienteDAO.isConnected(this)) {
+            new DownloadClientes().execute(HOST + URL);
+        } else {
+            Toast toast = Toast.makeText(this, "Rede indisponível!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+    private class DownloadClientes extends AsyncTask<String, Void, ArrayList<Cliente>> {
+
+        @Override
+        protected ArrayList<Cliente> doInBackground(String... strings) {
+            try {
+                return ClienteDAO.getClientes(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new ArrayList<Cliente>();
+        }
+
+        protected void onPostExecute(ArrayList<Cliente> clientes){
+            intent.putExtra(CLIENTES, clientes);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/br/usjt/ads/desmob/clienteads18/model/Cliente.java
@@ -0,0 +1,125 @@
+package br.usjt.ads.desmob.clienteads18.model;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+
+    private int id;
+    private String nome, email, diretor, data, genero, sinopse, pop, bilheteria, elenco;
+
+
+    public Cliente() {
+
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.diretor = diretor;
+        this.data = data;
+        this.genero = genero;
+        this.sinopse = sinopse;
+        this.pop = pop;
+        this.bilheteria = bilheteria;
+        this.elenco = elenco;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiretor(){
+        return diretor;}
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;}
+
+    public String getData(){
+        return data;}
+
+    public void setdata(String data) {
+        this.data = data;}
+
+    public String getGenero(){
+        return genero;
+    }
+
+    void setGenero(String genero){
+        this.genero = genero;
+    }
+
+    public String getPop(){
+        return pop;
+    }
+
+    void setPop(String pop){
+        this.pop = pop;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getBilheteria() {
+        return bilheteria;
+    }
+
+    public void setBilheteria(String bilheteria) {
+        this.bilheteria = bilheteria;
+    }
+
+    public String getElenco() {
+        return elenco;
+    }
+
+    public void setElenco(String elenco) {
+        this.elenco = elenco;
+    }
+
+
+    public String getFigura(){
+
+        String figura = email.replace('@', '_');
+        return figura.replace('.', '_');
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", diretor='" + diretor + '\'' +
+                ", data='" + data + '\'' +
+                ", genero='" + genero + '\'' +
+                ", sinopse='" + sinopse + '\'' +
+                ", pop='" + pop + '\'' +
+                ", bilheteria='" + bilheteria + '\'' +
+                ", elenco='" + elenco + '\'' +
+                '}';
+    }
+}
